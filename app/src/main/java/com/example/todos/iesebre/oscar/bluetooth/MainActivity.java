d1bb5c2eb5c133b98b3dc04534edc5fe2a83a29a
OscarDuranX <oscarduran@iesebre.com>
2016-02-16 19:46:02 +0100

    realitsa resposta segons la opcio

--- app/src/main/java/com/example/todos/iesebre/oscar/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/todos/iesebre/oscar/bluetooth/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
-    private static final String TAG = "BluetoothAdapterOscar";
+    private static final String TAG = "MainActivity";
     private BluetoothAdapter mBluetoothAdapter;
 
     @Override
@@ -19,26 +21,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this, "Bluetooth is Available!!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Bluetooth is Available!", Toast.LENGTH_LONG).show();
         }
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
@@ -49,21 +44,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch(requestCode){
+        switch (requestCode) {
             case REQUEST_ENABLE_BT:
                 if (resultCode == Activity.RESULT_OK) {
                     // Bluetooth is now enabled, so set up a chat session
-                    Toast.makeText(this, "Bluetooth is active", Toast.LENGTH_LONG).show();
-
+                    Toast.makeText(this, "Bluetooth is active!", Toast.LENGTH_LONG).show();
                 } else {
                     // User did not enable Bluetooth or an error occurred
                     Log.d(TAG, "BT not enabled");
-                    Toast.makeText(this,"Bluetooth no s'ha activat correctament!",
+                    Toast.makeText(this, "Bluetooth no s'ha activat correctament!",
                             Toast.LENGTH_SHORT).show();
                     this.finish();
                 }
                 break;
         }
+    }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.menu_discover:
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
